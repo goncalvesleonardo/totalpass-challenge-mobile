ec464bc556928de4d68cfd86bb4bec0e21c3bf29
Leonardo Gonçalves <leonardo.goncalves@compasso.com.br>
2022-04-23 09:36:50 -0300

    return text

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
+import {Text} from 'react-native';
 
 export const App: React.FC = () => {
-  return <></>;
+  return (
+    <>
+      <Text>Hello World</Text>
+    </>
+  );
 };
